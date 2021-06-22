a63a19775f2c6b070cc07d7a1fad8a1cd54e03f8
Ricky Gill <rickygill01@gmail.com>
2021-06-22 14:18:54 -0700

    UX fixes, images, play again button

--- app/src/main/java/com/example/theearthgame/TwoPlayerLoseActivity.java
+++ app/src/main/java/com/example/theearthgame/TwoPlayerLoseActivity.java
@@ -3,6 +3,7 @@ package com.example.theearthgame;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class TwoPlayerLoseActivity extends Activity {
@@ -15,6 +16,11 @@ public class TwoPlayerLoseActivity extends Activity {
         String country = intent.getStringExtra("country");
         TextView lose = (TextView) findViewById(R.id.lose);
         lose.setText(String.format("You both lose. The country was %s.", country));
+    }
+
+    public void onClickPlayAgain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
